cdb58473fcb8c497b27fecfba3488305f7121af9
mattIwago <daligdig.manuel19@gmail.com>
2026-03-13 00:58:23 +0800

    as123da
    flder

---
+++ winzelle/header.php
@@ -0,0 +1,29 @@
+
+<link rel="stylesheet" href="./assets/css/bootstrap/bootstrap.css">
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content ">
+      
+      <div class="modal-header">
+        <h5 class="modal-title">Modal Title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      
+      <div class="modal-body">
+        This is the modal content.
+      </div>
+      
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<script src="./assets/js/bootstrap/bootstrap.bundle.js"></script>
+<script src="./assets/js/jquery-3.7.1.min.js"></script>
+<script src="./assets/js/custom.js"></script>
